c5179c45c30c56cd7fc0387c3de1d14d886b8dd3
Z0ul0u25 <26393475+Z0ul0u25@users.noreply.github.com>
2024-11-30 12:58:25 -0500

    Modification du JS pour activité

--- js/script.js
+++ js/script.js
@@ -4,22 +4,35 @@ let images = null;
 let main = null;
 
 /**
+ * Fonction temporaire pour faire des tests.
+ */
+function fonctionBidon(){
+	// Appuyez sur F12 dans votre navigateur web pour voir la console
+	console.log("Je suis une fonction bidon");
+}
+
+/**
  * Affiche une image en plein écran.
  * @param {HTMLElement} image miniature de l'image à afficher en plein écran
  */
-function AfficherImagePleinEcran(image) {
+function afficherImagePleinEcran(image) {
 	// On crée un élément <div> qui va contenir l'image en plein écran
 	let ImageSuperpose = document.createElement('div');
 	ImageSuperpose.id = 'superposition';
+
+	// On ajoute un écouteur d'événement sur l'élément <div> pour le fermer lorsqu'on cliquera dessus
 	ImageSuperpose.addEventListener('click', function() {
 		// On retire l'élément <div> de la superposition quand on clique dessus
 		main.removeChild(ImageSuperpose);
 	}, false);
 
+	// On ajoute l'élément <div> à la fin de la balise <main>
 	main.appendChild(ImageSuperpose);
 
 	// On crée un élément <img> qui va contenir l'image en plein écran
 	let img = document.createElement('img');
+	// On remplace le chemin de l'image miniature par le chemin de l'image en taille originale.
+	// ? Il est important que les images est le même nom pour que cela fonctionne.
 	img.src = image.src.replace('/miniatures/', '/originales/');
 	img.alt = image.alt;
 	ImageSuperpose.appendChild(img);
@@ -38,14 +51,19 @@ function AfficherImagePleinEcran(image) {
  * Lancé une fois que le DOM est chargé.
  */
 function initialisation() {
-	images = document.querySelectorAll('#galerie img');
 	main = document.querySelector('main');
 
+	// On récupère toutes les images de la galerie dans un tableau
+	images = document.querySelectorAll('#galerie img');
+
 	// On ajoute un écouteur d'événement sur chaque image
-	images.forEach(function(image) {
-		image.addEventListener('click', function() {
-			// On appelle la fonction showImage en lui passant l'image cliquée en paramètre
-			AfficherImagePleinEcran(image);
+	images.forEach((image) => {
+		// 'image' est l'élément <img> courant
+		// TODO: Changez le nom de l'événement pour qu'il corresponde à la bonne action
+		image.addEventListener('nom_evenement', function() {
+			// TODO: Appelez la bonne fonction pour afficher l'image en plein écran
+			// * Ne pas oublier de passer 'image' en paramètre (entre les parenthèses)
+			fonctionBidon();
 		}, false);
 	});
 }
